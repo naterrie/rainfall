c386dda3edc1bcf4f47338c3aefe225066149824
bkaboa <73066722+bkaboa@users.noreply.github.com>
2025-11-17 16:21:40 +0100

    [ADD] bonus0

--- level2/source.c
+++ level2/source.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void pFunction(void)
+{
+    char var2[76];
+
+    fflush(stdout);
+    gets(var2);
+    if ((&var2 & 0xb0000000) == 0xb0000000)
+    {
+        printf("(%p)\n", *var2);
+        _exit(1);
+    }
+    puts(var2);
+    strdup(var2);
+    return;
+}
+
 void main(void)
 {
+    pFunction();
+    return;
 }
